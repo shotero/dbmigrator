32a4d56737e77fde2d671ff824cb92d094d5d01e
lemon-chiffon <110283237+lemon-chiffon@users.noreply.github.com>
2022-08-15 23:22:48 +0800

    feat: down

--- commands/down.js
+++ commands/down.js
@@ -1,21 +1,61 @@
+import fs from 'node:fs';
+import path from 'node:path';
 import { program } from '../main.js';
 import { logger } from '../logger.js';
 import { pool } from '../adapters.js';
 import config from '../config.js';
+import {
+  apply,
+  checksum,
+  cut,
+  ensureMigrationTable,
+  getApplied,
+  getMigrationTable,
+  handleError
+} from '../utils.js';
 
 function load() {
   program
     .command('down')
     .description('rollback migrations')
     .argument('<migration>', 'migration to revert back to')
-    .action((migration) => {
+    .action((target) => {
       console.log('rollback');
-      down(migration, config, pool);
+      down(target, config, pool);
     });
 }
 
-function down(migration, settings, db) {
-  console.log('revert migrations');
+async function down(target, settings, db) {
+  try {
+    const migrationTable = getMigrationTable(settings);
+    ensureMigrationTable(db, migrationTable);
+    const migrations = await getApplied(db, migrationTable);
+    const rollbackList = cut(
+      migrations.rows.map((i) => i.filename),
+      target
+    );
+    const statement = getQuery(rollbackList, settings);
+    await apply(db, statement);
+    await db.end();
+    logger.success('Successfully reverted');
+  } catch (e) {
+    handleError(e);
+  }
+}
+
+function getQuery(migrations, settings) {
+  const migrationTable = getMigrationTable(settings);
+  let sql = '';
+  logger.log(`List of migrations being reversed:`);
+  for (const migration of migrations) {
+    logger.log(`${migration}`);
+    const filePath = settings.paths.down;
+    const content =
+      fs.readFileSync(path.join(filePath, migration), 'utf8') || '';
+    sql += content;
+    sql += `DELETE FROM ${migrationTable} WHERE filename = '${migration}';`;
+  }
+  return sql;
 }
 
 export { down, load };
